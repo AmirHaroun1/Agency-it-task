fe324fb57e997cecff3efd1ddf887cb93c4e1a62
Amir Haroun <60839505+AmirHaroun1@users.noreply.github.com>
2021-09-08 16:52:33 +0200

    Adding Auth Routes to Api Routes

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,17 @@
 <?php
 
+use App\Http\Controllers\ApiControllers\Auth\LoginController;
+use App\Http\Controllers\ApiControllers\Auth\LogoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
+Route::group(['prefix'=>'/api/v1'],function(){
 
+    Route::post('/login',[LoginController::class,'login']);
+    Route::get('/logout',[LogoutController::class,'logout']);
+    Route::group(['middleware'=>'auth:sanctum'],function(){
 
-Route::group(['middleware'=>'auth:sanctum','prefix'=>'/api/v1'],function(){
-
+    });
 
 });
