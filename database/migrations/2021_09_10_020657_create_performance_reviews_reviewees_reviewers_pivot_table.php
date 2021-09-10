95f4dc47b0a4a534dfd7232d877f73cfc47d4e21
Amir Haroun <60839505+AmirHaroun1@users.noreply.github.com>
2021-09-10 04:14:14 +0200

    Add Performance Reviews Table

---
+++ database/migrations/2021_09_10_020657_create_performance_reviews_reviewees_reviewers_pivot_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerformanceReviewsRevieweesReviewersPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('performance_reviews_reviewees_reviewers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('reviewee_id')->nullable();
+            $table->foreign('reviewee_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->foreignId('reviewer_id')->nullable();
+            $table->foreign('reviewer_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->text('feedback')->nullable();
+            $table->boolean('status')->default(0); // 0 for Pending - 1 for Submitted
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('performance_reviews_reviewees_reviewers');
+    }
+}
